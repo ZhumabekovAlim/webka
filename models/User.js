4ffc4e3474dff6de730209514c2b9b5566e79366
Zhumabekov04 <alim.zhumabekov@bk.ru>
2023-12-18 10:37:10 +0600

    first commit

---
+++ models/User.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profile: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Profile',
+            },
+        ],
+    },
+    { timestamps: true },
+)
+
+export default mongoose.model('User', UserSchema)
